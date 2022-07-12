420c60367bc3311f9a5a86e54b2015fa1c7ff0f8
stefan-ax <stefan.obada@yahoo.com>
2022-07-12 23:41:59 +0100

    lego adapt

--- configs/custom/OwnLego.py
+++ configs/custom/OwnLego.py
@@ -3,9 +3,20 @@
 _base_ = './default_ubd_inward_facing.py'
 
 # name your scene
-expname = 'MyScene'
+expname = 'OwnLego'
 
 data = dict(
     datadir='./data/custom/lego-custom/dense',  # path to the root of your images
     factor=2,                               # the training images resolution
+    dataset_type='blender',
+    white_bkgd=True
+)
+
+fine_train = dict(
+    N_iters=5000,
+)
+
+fine_model_and_render = dict(
+    num_voxels=160**3,
+    num_voxels_base=160**3
 )
